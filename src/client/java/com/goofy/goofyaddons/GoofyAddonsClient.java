7df68ceab3b8517483bc08407383d3597a6f2d0d
mscreepss <mscreepss@gmail.com>
2026-08-22 22:49:53 +0300

    Remove GoofyOverlay registration and keybinds
    Removed unused GoofyOverlay and related keybinds.

--- src/client/java/com/goofy/goofyaddons/GoofyAddonsClient.java
+++ src/client/java/com/goofy/goofyaddons/GoofyAddonsClient.java
@@ -7,8 +7,6 @@ import com.goofy.goofyaddons.features.FeatureManager;
 import com.goofy.goofyaddons.features.economy.EconomyTracker;
 import com.goofy.goofyaddons.keybinds.GoofyKeybinds;
 import com.goofy.goofyaddons.render.hud.EconomyHud;
-import com.goofy.goofyaddons.render.hud.GoofyOverlay;
-import com.goofy.goofyaddons.utils.ChatUtils;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
@@ -18,34 +16,19 @@ public class GoofyAddonsClient implements ClientModInitializer {
     public void onInitializeClient() {
         GoofyConfig.load();
         ChatHook.register();
-        GoofyKeybinds.register();
         EconomyTracker.register();
         EconomyHud.register();
-        GoofyOverlay.register();
+
+        // ESKİ ARAYÜZ (G tuşu ile açılan GoofyOverlay) ARTIK KAYITLI DEĞİL.
+        // Sınıf silinmedi, sadece devre dışı bırakıldı: ileride tekrar istersen
+        // aşağıdaki satırı geri açman yeterli. Yerini M ile açılan GoofyScreen aldı.
+        // GoofyOverlay.register();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             FailsafeManager.INSTANCE.onTick();
             FeatureManager.INSTANCE.onTick();
-            // Uptime sayacı: makro aktif değilse süre eklemez, sadece zaman damgasını günceller.
             EconomyTracker.onTick();
-
-            while (GoofyKeybinds.reloadConfigKey.consumeClick()) {
-                GoofyConfig.load();
-            }
-
-            while (GoofyKeybinds.startKey.consumeClick()) {
-                FeatureManager.INSTANCE.start("BazaarFlipper");
-            }
-            while (GoofyKeybinds.stopKey.consumeClick()) {
-                FeatureManager.INSTANCE.stop();
-            }
-            while (GoofyKeybinds.toggleOverlayKey.consumeClick()) {
-                GoofyOverlay.toggle();
-            }
-            while (GoofyKeybinds.toggleEconomyModeKey.consumeClick()) {
-                EconomyTracker.toggleMode();
-                ChatUtils.clientMessage("Economy HUD: " + EconomyTracker.getModeLabel() + " modu");
-            }
+            GoofyKeybinds.onTick();
         });
     }
 }
