809b0b3ced63cbac8c78a59b8fdfc20782b21130
mscreepss <mscreepss@gmail.com>
2026-08-24 02:13:28 +0300

    Update build tag and initialize new features
    Updated build tag from UI-5 to UI-6 and initialized new features.

--- src/client/java/com/goofy/goofyaddons/GoofyAddonsClient.java
+++ src/client/java/com/goofy/goofyaddons/GoofyAddonsClient.java
@@ -1,13 +1,17 @@
 package com.goofy.goofyaddons;
 
+import com.goofy.goofyaddons.config.BookPresets;
 import com.goofy.goofyaddons.config.GoofyConfig;
 import com.goofy.goofyaddons.event.ChatHook;
 import com.goofy.goofyaddons.failsafes.FailsafeManager;
 import com.goofy.goofyaddons.features.FeatureManager;
+import com.goofy.goofyaddons.features.bookflipper.helper.ItemCatalog;
+import com.goofy.goofyaddons.features.bookflipper.helper.TradeHistory;
 import com.goofy.goofyaddons.features.economy.EconomyTracker;
 import com.goofy.goofyaddons.keybinds.GoofyKeybinds;
 import com.goofy.goofyaddons.keybinds.KeyAction;
 import com.goofy.goofyaddons.render.hud.EconomyHud;
+import com.goofy.goofyaddons.utils.ActionLog;
 import com.goofy.goofyaddons.utils.ChatUtils;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -19,7 +23,7 @@ public class GoofyAddonsClient implements ClientModInitializer {
      * Dünyaya girince sohbette görünmüyorsa çalışan jar ESKİDİR - yeni kodun
      * hiçbiri devrede değil demektir.
      */
-    private static final String BUILD_TAG = "UI-5";
+    private static final String BUILD_TAG = "UI-6";
 
     private static boolean greeted = false;
 
@@ -28,9 +32,14 @@ public class GoofyAddonsClient implements ClientModInitializer {
         System.out.println("[GoofyAddons] client init - build " + BUILD_TAG);
 
         GoofyConfig.load();
+        BookPresets.load();
+        ActionLog.init();
         ChatHook.register();
         EconomyTracker.register();
+        TradeHistory.register();
         EconomyHud.register();
+        // Kitap ID kataloğu: arka planda indirilir, oyunun açılışını bekletmez.
+        ItemCatalog.init();
 
         // ESKİ ARAYÜZ (G tuşuyla açılan GoofyOverlay) kayıtlı değil.
         // Yerini M ile açılan GoofyScreen aldı. Geri istersen bu satırı aç:
@@ -47,6 +56,8 @@ public class GoofyAddonsClient implements ClientModInitializer {
             FailsafeManager.INSTANCE.onTick();
             FeatureManager.INSTANCE.onTick();
             EconomyTracker.onTick();
+            TradeHistory.onTick();
+            ActionLog.onTick();
             GoofyKeybinds.onTick();
         });
     }
