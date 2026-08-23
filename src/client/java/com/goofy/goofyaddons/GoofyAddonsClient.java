0806aca179da4aabe439449c5a024b01d20f14bc
mscreepss <mscreepss@gmail.com>
2026-08-23 21:05:25 +0300

    Add build tag and update comments in GoofyAddonsClient
    Added a build tag for version identification and updated comments regarding the old interface.

--- src/client/java/com/goofy/goofyaddons/GoofyAddonsClient.java
+++ src/client/java/com/goofy/goofyaddons/GoofyAddonsClient.java
@@ -6,25 +6,44 @@ import com.goofy.goofyaddons.failsafes.FailsafeManager;
 import com.goofy.goofyaddons.features.FeatureManager;
 import com.goofy.goofyaddons.features.economy.EconomyTracker;
 import com.goofy.goofyaddons.keybinds.GoofyKeybinds;
+import com.goofy.goofyaddons.keybinds.KeyAction;
 import com.goofy.goofyaddons.render.hud.EconomyHud;
+import com.goofy.goofyaddons.utils.ChatUtils;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class GoofyAddonsClient implements ClientModInitializer {
 
+    /**
+     * TEŞHİS DAMGASI: Bu satır, oyunda hangi jar'ın çalıştığını kesinleştirir.
+     * Dünyaya girince sohbette görünmüyorsa çalışan jar ESKİDİR - yeni kodun
+     * hiçbiri devrede değil demektir.
+     */
+    private static final String BUILD_TAG = "UI-4";
+
+    private static boolean greeted = false;
+
     @Override
     public void onInitializeClient() {
+        System.out.println("[GoofyAddons] client init - build " + BUILD_TAG);
+
         GoofyConfig.load();
         ChatHook.register();
         EconomyTracker.register();
         EconomyHud.register();
 
-        // ESKİ ARAYÜZ (G tuşu ile açılan GoofyOverlay) ARTIK KAYITLI DEĞİL.
-        // Sınıf silinmedi, sadece devre dışı bırakıldı: ileride tekrar istersen
-        // aşağıdaki satırı geri açman yeterli. Yerini M ile açılan GoofyScreen aldı.
+        // ESKİ ARAYÜZ (G tuşuyla açılan GoofyOverlay) kayıtlı değil.
+        // Yerini M ile açılan GoofyScreen aldı. Geri istersen bu satırı aç:
         // GoofyOverlay.register();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (!greeted && client.player != null) {
+                greeted = true;
+                ChatUtils.clientMessage("GoofyAddons build " + BUILD_TAG + " yuklendi  |  "
+                        + KeyAction.MENU.keyName() + " = menu  |  "
+                        + KeyAction.HUD_MODE.keyName() + " = HUD modu");
+            }
+
             FailsafeManager.INSTANCE.onTick();
             FeatureManager.INSTANCE.onTick();
             EconomyTracker.onTick();
