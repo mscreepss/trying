ca312ecd04186accaef0e13b00fac6ee04e0206a
mscreepss <mscreepss@gmail.com>
2026-08-24 03:11:08 +0300

    Update build tag and register new HUDs

--- src/client/java/com/goofy/goofyaddons/GoofyAddonsClient.java
+++ src/client/java/com/goofy/goofyaddons/GoofyAddonsClient.java
@@ -11,6 +11,8 @@ import com.goofy.goofyaddons.features.economy.EconomyTracker;
 import com.goofy.goofyaddons.keybinds.GoofyKeybinds;
 import com.goofy.goofyaddons.keybinds.KeyAction;
 import com.goofy.goofyaddons.render.hud.EconomyHud;
+import com.goofy.goofyaddons.render.hud.StateHud;
+import com.goofy.goofyaddons.render.hud.TaskHud;
 import com.goofy.goofyaddons.utils.ActionLog;
 import com.goofy.goofyaddons.utils.ChatUtils;
 import net.fabricmc.api.ClientModInitializer;
@@ -23,7 +25,7 @@ public class GoofyAddonsClient implements ClientModInitializer {
      * Dünyaya girince sohbette görünmüyorsa çalışan jar ESKİDİR - yeni kodun
      * hiçbiri devrede değil demektir.
      */
-    private static final String BUILD_TAG = "UI-6";
+    private static final String BUILD_TAG = "UI-7";
 
     private static boolean greeted = false;
 
@@ -37,20 +39,20 @@ public class GoofyAddonsClient implements ClientModInitializer {
         ChatHook.register();
         EconomyTracker.register();
         TradeHistory.register();
+        // Uc HUD da kayitli; hangisinin gorunecegi config'ten (HUD sayfasi) okunur.
         EconomyHud.register();
+        TaskHud.register();
+        StateHud.register();
+
         // Kitap ID kataloğu: arka planda indirilir, oyunun açılışını bekletmez.
         ItemCatalog.init();
-
-        // ESKİ ARAYÜZ (G tuşuyla açılan GoofyOverlay) kayıtlı değil.
-        // Yerini M ile açılan GoofyScreen aldı. Geri istersen bu satırı aç:
-        // GoofyOverlay.register();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (!greeted && client.player != null) {
                 greeted = true;
-                ChatUtils.clientMessage("GoofyAddons build " + BUILD_TAG + " yuklendi  |  "
-                        + KeyAction.MENU.keyName() + " = menu  |  "
-                        + KeyAction.HUD_MODE.keyName() + " = HUD modu");
+                ChatUtils.clientMessage("GoofyAddons build " + BUILD_TAG + " loaded  |  "
+                        + KeyAction.MENU.keyName() + " = open / close menu  |  "
+                        + KeyAction.HUD_MODE.keyName() + " = profit HUD mode");
             }
 
             FailsafeManager.INSTANCE.onTick();
